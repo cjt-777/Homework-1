928232d12b587b93032f046abcb653eac9ee85eb
Randy Chang <0116randy@gmail.com>
2024-03-27 20:37:05 +0800

    6

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,21 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
-
+    mapping(address => bool) public registeredUsers;
+    uint256 public totalRegisteredUsers;
+    
+    // Constructor
+    constructor() {
+        totalRegisteredUsers = 0;
+    }
+    event UserRegistered(address indexed user);
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        require(!registeredUsers[msg.sender], "Already registered");
+        
+        registeredUsers[msg.sender] = true;
+        totalRegisteredUsers++;
+        emit UserRegistered(msg.sender);
     }
 }
 
